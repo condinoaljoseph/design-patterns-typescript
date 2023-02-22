9974e16914fae466a1d5f77d526108e08d33d383
Al Joseph Condino <condinoaljoseph@gmail.com>
2023-02-22 14:22:17 +0800

    refactor code

--- src/Singleton/Conceptual/index.ts
+++ src/Singleton/Conceptual/index.ts
@@ -1,27 +1,41 @@
 class Database {
     private static instance: Database;
+    private connected = false;
+  
     private constructor() {}
-
-    public static getInstance() {
-        if(!Database.instance) {
-            Database.instance = new Database();
-        }
-
-        return Database.instance;
+  
+    public static getInstance(): Database {
+      if (!Database.instance) {
+        Database.instance = new Database();
+      }
+      return Database.instance;
     }
-
-    connect() {
-        return 'Database connected';
+  
+    public static connect(): string {
+      const instance = Database.getInstance();
+      instance.connected = true;
+      return 'Database connected';
+    }
+  
+    public static isConnected(): boolean {
+      const instance = Database.getInstance();
+      return instance.connected;
     }
 }
-
+  
 const db1 = Database.getInstance();
 const db2 = Database.getInstance();
 
 if (db1 === db2) {
-    console.log('Working: Both databases has the same instance')
+    console.log('Working: Both databases have the same instance');
 } else {
-    console.log('Failing: Databases has different instance');
+    console.log('Failing: Databases have different instances');
 }
 
-console.log(db1.connect());
+console.log(Database.connect());
+
+if (Database.isConnected()) {
+    console.log('Database is connected');
+} else {
+    console.log('Database is not connected');
+}
